1c6a1dd605697343354864a2766b7ca6e9f1b902
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-05 14:40:07 +0200

    :zap: installed axios

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Row from './Row.js';
+import requests from './requests.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+
     </div>
   );
 }
