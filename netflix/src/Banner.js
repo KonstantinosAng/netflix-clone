e8bc69145360844eae611c42c274ac70b83e11e4
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-05 16:24:14 +0200

    :zap: added tab icon and banner style

--- netflix/src/Banner.js
+++ netflix/src/Banner.js
@@ -16,18 +16,20 @@ function Banner() {
     fetchData();
   }, []);
 
-  console.log(movie);
+  function truncate_string(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
   return (
     <header className="banner"
       style={{backgroundImage: `url(${requests.baseUrl}${movie?.backdrop_path})`}}>
       <div className="banner__contents">
-        <h1> {movie?.title || movie?.name || movie?.original_name} </h1>
+        <h1 className="banner__title"> {movie?.title || movie?.name || movie?.original_name} </h1>
         <div className="banner__buttons">
           <button className="banner__button"> Play </button> 
           <button className="banner__button"> My List </button> 
         </div>
-        <h1 className="banner__description"> {movie?.overview} </h1>
+        <h1 className="banner__description"> {truncate_string(movie?.overview, 150)} </h1>
       </div>
     </header>
   )
