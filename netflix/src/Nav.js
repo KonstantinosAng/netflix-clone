2ca8672995767574afb2d47daa60ab4dd1d74604
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-06 22:32:20 +0200

    :zap: nav style

--- netflix/src/Nav.js
+++ netflix/src/Nav.js
@@ -4,6 +4,7 @@ import './Nav.css';
 function Nav() {
 
   const [show, handleShow] = useState(false);
+  const [showSearch, setShowSearch] = useState(false)
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -17,6 +18,14 @@ function Nav() {
       window.removeEventListener("scroll", useEffect);
     };
   }, []);
+
+  function handleClick() {
+    if (showSearch) {
+      setShowSearch(false);
+    } else {
+      setShowSearch(true);
+    }
+  }
 
   const Reload = () => {
     window.location.reload();
@@ -40,7 +49,11 @@ function Nav() {
       <i className="fa fa-bell"></i>
       <i class="fa fa-gift"></i>
       <h5 className="nav__children"> CHILDREN </h5>
-      <i class="fa fa-search"></i>
+      <i class="fa fa-search" onClick={handleClick}></i>
+      <div className={`search__bar ${showSearch && "search__barShow"}`}>
+        <input type="text" className="search__bar__input" placeholder="Titles, people, genres" />  
+        <i class="fa fa-search" onClick={handleClick}></i>
+      </div>
       <img 
       className="nav__avatar"
       src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F666011864883662849%2FgQwK8LLw.jpg&f=1&nofb=1"
