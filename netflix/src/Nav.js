fab9ffd5d9daceca750613d6c608302efa950338
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-06 20:16:21 +0200

    styling header

--- netflix/src/Nav.js
+++ netflix/src/Nav.js
@@ -30,6 +30,13 @@ function Nav() {
       src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
       alt="Netflix Logo"
       />
+      <div className="nav__row">
+        <h5 className="nav__home"> Home </h5>
+        <h5 className="nav__series"> Series </h5>
+        <h5 className="nav__films"> Films </h5>
+        <h5 className="nav__news"> News & Popular </h5>
+        <h5 className="nav__list"> My List </h5>
+      </div>
       <img 
       className="nav__avatar"
       src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F666011864883662849%2FgQwK8LLw.jpg&f=1&nofb=1"
