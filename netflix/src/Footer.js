7c72f3e4cf53d5778c3898fdeaa83b9779490d3f
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-06 13:53:40 +0200

    :zap: footer styling

--- netflix/src/Footer.js
+++ netflix/src/Footer.js
@@ -10,25 +10,37 @@ function Footer() {
         <i className="social fa fa-youtube-play" />
       </div>
       <div className="footer__row">
-        <h5> Audio and Subtitles </h5>
-        <h5> Audio Description </h5>
-        <h5> Help Centre </h5>
-        <h5> Gift Cards </h5>
+        <div className="footer__col">
+          <h5> Audio and Subtitles </h5>
+          <h5> Media Centre </h5>
+          <h5> Privacy </h5>
+          <h5> Contact Us </h5>
+        </div>
+        <div className="footer__col">
+          <h5> Audio Description </h5>
+          <h5> Investor Relations </h5>
+          <h5> Legal Notices </h5>
+          <h4>  </h4>
+        </div>
+        <div className="footer__col">
+          <h5> Help Centre </h5>
+          <h5> Jobs </h5>
+          <h5> Cookie Preferences </h5>
+          <h4>  </h4>
+        </div>
+        <div className="footer__col">
+          <h5> Gift Cards </h5>
+          <h5> Terms of Use </h5>
+          <h5> Corporate Information </h5>
+          <h4>  </h4>
+        </div>
       </div>
       <div className="footer__row">
-        <h5> Media Centre </h5>
-        <h5> Investor Relations </h5>
-        <h5> Jobs </h5>
-        <h5> Terms of Use </h5>
+        <button className="footer__button"> Service Code </button>
       </div>
       <div className="footer__row">
-        <h5> Privacy </h5>
-        <h5> Legal Notices </h5>
-        <h5> Cookie Preferences </h5>
-        <h5> Corporate Information </h5>
-      </div>
-      <div className="footer__row">
-        <h5> Contact Us </h5>
+        <span> © 1997-2021 Netflix, Inc. </span>
+        <span> &lrm; </span>
       </div>
     </div>
   )
