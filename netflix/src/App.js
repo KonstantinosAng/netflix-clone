34656ab96b7eb163d4a4b8da2fd16f51af5e43b5
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-07 18:24:45 +0200

    changed directories, added switch & router

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,26 +1,15 @@
-import './App.css';
-import Row from './Row.js';
-import Banner from './Banner.js';
-import Nav from './Nav.js';
-import Footer from './Footer.js';
-import requests from './requests.js';
+import Browse from './Browse.js';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <Nav fetchUrl={requests.fetchTrending} />
-      <Banner />
-      <div className="rows">
-        <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
-        <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
-        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
-      </div>
-      <Footer />
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Browse} />
+          <Route path='/browse' component={Browse} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ netflix/src/Browse.js
@@ -0,0 +1,28 @@
+import './Browse.css';
+import Row from './components/Row.js';
+import Banner from './components/Banner.js';
+import Nav from './components/Nav.js';
+import Footer from './components/Footer.js';
+import requests from './components/requests.js';
+
+function Browse() {
+  return (
+    <div className="browse">
+      <Nav fetchUrl={requests.fetchTrending} />
+      <Banner />
+      <div className="rows">
+        <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Browse;
