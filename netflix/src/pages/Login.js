501da3301ac04f1911e5da3d4c2865c3f22c1e6e
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-08 13:22:38 +0200

    styling login page

--- netflix/src/pages/Login.js
+++ netflix/src/pages/Login.js
@@ -9,7 +9,6 @@ function Login() {
 
   return (
     <div className="login__root">
-      <div className="login__fade__top"></div>
       <div className="login__banner">
         <img
         className="login__logo"
@@ -17,6 +16,25 @@ function Login() {
         alt="Netflix Logo"
         />
         <button className="login__button"> <h3> Sign In </h3> </button>
+      </div>
+      <div className="login__fade__top"></div>
+      <div className="login__body">
+        <div className="login__body__row">
+          <h1 className="login__body__header"> Unlimited Movies, TV shows, and more. </h1>
+        </div>
+        <div className="login__body__row">
+          <h2 className="login__body__row2"> Watch anywhere. Cancel anytime. </h2>
+        </div>
+        <div className="login__body__row">
+          <h3 className="login__body__row2 row3"> Ready to watch? Enter your email to create or restart your membership. </h3>
+        </div>
+        <div className="login__body__row col">
+          <input className="login__body__email" type="email" placeholder="Email address" />
+          <button className="login__body__button"> TRY 30 DAYS FREE > </button>
+        </div>
+        <div className="login__body__row">
+          <h3 className="login__body__row2 row3"> Only new members are eligible for this offer. </h3>
+        </div>
       </div>
     </div>
   )
