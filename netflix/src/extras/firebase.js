7c7b8a689fceb094773ff0404330b1a73509aecd
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-09 22:36:49 +0200

    reverting to commit <92c207c4646b1df74e67f335509396e4e2848300>

---
+++ netflix/src/extras/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAaSs1VnMm1Lf8t_25vsKuYWSYJgYtXWVY",
+  authDomain: "netflix-clone-46ddf.firebaseapp.com",
+  projectId: "netflix-clone-46ddf",
+  storageBucket: "netflix-clone-46ddf.appspot.com",
+  messagingSenderId: "327331761514",
+  appId: "1:327331761514:web:9b25f944bc96f5998da913",
+  measurementId: "G-QML9CZH3JX"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
---
+++ netflix/src/extras/requests.js
@@ -0,0 +1,15 @@
+const TMDB_API_KEY="b12c4ce548390f09a78b7fc6df9b191f";
+
+const requests = {
+  baseUrl: `https://image.tmdb.org/t/p/original/`,
+  fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${TMDB_API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${TMDB_API_KEY}&with_networks=213`,
+  fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${TMDB_API_KEY}&language=en-US`,
+  fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_genres=28`,
+  fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_genres=99`
+}
+
+export default requests;
