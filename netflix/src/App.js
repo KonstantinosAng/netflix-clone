f88464cc3d281a30ed21d5976cc456ce821ccd82
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-19 16:19:25 +0200

    removed unecessary imports

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -4,8 +4,6 @@ import Home from './pages/Home.js';
 import Profile from './pages/Profile.js';
 import LoadingPage from './pages/LoadingPage.js';
 import NotFound from './pages/NotFound.js';
-import SignIn from './pages/SignIn.js';
-import SignUp from './pages/SignUp.js';
 import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom';
 import React, { useEffect } from 'react';
 import { auth } from './extras/firebase.js';
